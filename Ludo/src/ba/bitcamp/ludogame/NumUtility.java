fde13cc2504aacdf30b5838cc140166acdaf422e
boristomic <boris.tomic@bitcamp.ba>
2015-08-12 11:28:37 +0200

    added comments to NumUtility class

--- Ludo/src/ba/bitcamp/ludogame/NumUtility.java
+++ Ludo/src/ba/bitcamp/ludogame/NumUtility.java
@@ -2,8 +2,20 @@ package ba.bitcamp.ludogame;
 
 import java.util.Random;
 
+/**
+ * Utility class <tt>NumUtility</tt> used to call static method that manipulate
+ * number values.
+ * 
+ * @author boris.tomic
+ *
+ */
 public class NumUtility {
 
+	/**
+	 * Static method returns random number from 1 to 6 used to set dice value.
+	 * 
+	 * @return <code>int</code> type value from 1 to 6 randomly given
+	 */
 	public static int getRandomNumber() {
 		Random rand = new Random();
 		int option = rand.nextInt(6) + 1;
